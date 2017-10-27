2adec52df73881f18ff449363ac3548192b1e6b8
r05229014 <r05229014@ntu.edu.tw>
2017-10-27 20:51:30 +0800

    finally

---
+++ hw2/hw2_generative.sh
@@ -0,0 +1,1 @@
+python hw2_generative.py $3 $4 $5 $6
--- hw2/hw2_logistic.py
+++ hw2/hw2_logistic.py
@@ -5,6 +5,11 @@ import argparse
 from math import log, floor
 import pandas as pd
 
+x_train_path = sys.argv[1]
+y_train_path = sys.argv[2]
+test_data_path = sys.argv[3]
+out_path = sys.argv[4]
+
 def load_data(train_data_path, train_label_path, test_data_path):
     X_train = pd.read_csv(train_data_path, sep=',', header=0)
     X_train = np.array(X_train.values)
@@ -48,6 +53,16 @@ def split_valid_set(X_all, Y_all, percentage):
 def sigmoid(z):
     res = 1 / (1.0 + np.exp(-z))
     return np.clip(res, 1e-8, 1-(1e-8))
+
+def valid(w, b, X_valid, Y_valid):
+    valid_data_size = len(X_valid)
+
+    z = (np.dot(X_valid, np.transpose(w)) + b)
+    y = sigmoid(z)
+    y_ = np.around(y)
+    result = (np.squeeze(Y_valid) == y_)
+    print('Validation acc = %f' % (float(result.sum()) / valid_data_size))
+    return
 
 def train(X_all, Y_all, save_dir):
     # Split a 10%-validation set from the training set
@@ -101,7 +116,7 @@ def train(X_all, Y_all, save_dir):
 
     return
 
-def infer(X_test, save_dir, output_dir):
+def infer(X_test, save_dir, output_path):
     test_data_size = len(X_test)
 
     # Load parameters
@@ -114,10 +129,11 @@ def infer(X_test, save_dir, output_dir):
     y = sigmoid(z)
     y_ = np.around(y)
 
-    print('=====Write output to %s =====' % output_dir)
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
-    output_path = os.path.join(output_dir, 'log_prediction.csv')
+    print('=====Write output to %s =====' % output_path)
+    #if not os.path.exists(output_dir):
+        #os.mkdir(output_dir)
+    #output_path = os.path.join(output_dir, 'log_prediction.csv')
+    output_path = output_path
     with open(output_path, 'w') as f:
         f.write('id,label\n')
         for i, v in  enumerate(y_):
@@ -125,42 +141,20 @@ def infer(X_test, save_dir, output_dir):
 
     return
 
-def main(opts):
+
+def main():
+    save_dir = 'logistic_params'
+    output_path = out_path
     # Load feature and label
-    X_all, Y_all, X_test = load_data(opts.train_data_path, opts.train_label_path, opts.test_data_path)
+    X_all, Y_all, X_test = load_data(x_train_path, y_train_path, 
+				     test_data_path)
     # Normalization
     X_all, X_test = normalize(X_all, X_test)
 
     # To train or to infer
-    if opts.train:
-        train(X_all, Y_all, opts.save_dir)
-    elif opts.infer:
-        infer(X_test, opts.save_dir, opts.output_dir)
-    else:
-        print("Error: Argument --train or --infer not found")
-    return
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Logistic Regression with Gradient Descent Method')
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('--train', action='store_true', default=False,
-                        dest='train', help='Input --train to Train')
-    group.add_argument('--infer', action='store_true',default=False,
-                        dest='infer', help='Input --infer to Infer')
-    parser.add_argument('--train_data_path', type=str,
-                        default='feature/X_train', dest='train_data_path',
-                        help='Path to training data')
-    parser.add_argument('--train_label_path', type=str,
-                        default='feature/Y_train', dest='train_label_path',
-                        help='Path to training data\'s label')
-    parser.add_argument('--test_data_path', type=str,
-                        default='feature/X_test', dest='test_data_path',
-                        help='Path to testing data')
-    parser.add_argument('--save_dir', type=str,
-                        default='logistic_params/', dest='save_dir',
-                        help='Path to save the model parameters')
-    parser.add_argument('--output_dir', type=str,
-                        default='logistic_output/', dest='output_dir',
-                        help='Path to save the model parameters')
-    opts = parser.parse_args()
-    main(opts)
+    train(X_all, Y_all, save_dir)
+    infer(X_test, save_dir, output_path)
+
+main()
+    
