40231878f054409ab6bb823fd5d9221389b138eb
r05229014 <r05229014@ntu.edu.tw>
2018-01-12 22:25:52 +0800

    update

--- hw6/pca.py
+++ hw6/pca.py
@@ -2,23 +2,26 @@ from skimage import io
 import skimage
 import numpy as np
 import sys
+import os
 
 path1 = sys.argv[1]
 path2 = sys.argv[2]
 
 def reconstruction(img):
-    y = img - X_mean
-    y = np.dot(y,U)
-    uuu = np.zeros(600*600*3)
+	img = io.imread(img)
+	img = img.flatten()
+	y = img - X_mean
+	y = np.dot(y,U)
+	uuu = np.zeros(600*600*3)
     
-    for i in range(4):
-        uuu += y[i] * U[:,i]
-    new = (uuu + X_mean)
-    new -= np.min(new)
-    new /= np.max(new)
-    new = (new*255).astype(np.uint8)
-    new = new.reshape(600,600,3)
-    return new
+	for i in range(4):
+		uuu += y[i] * U[:,i]
+	new = (uuu + X_mean)
+	new -= np.min(new)
+	new /= np.max(new)
+	new = (new*255).astype(np.uint8)
+	new = new.reshape(600,600,3)
+	return new
 
 img_data = []
 for i in range(415):
@@ -39,6 +42,6 @@ x = np.zeros(X.shape)
 for i in range(X.shape[1]):
     x[:,i] = X[:,i] - X_mean
 U, s, V = np.linalg.svd(x, full_matrices=False)
-
-ff = reconstruction(X[:,1])
-io.imsave(path2, ff)
+print(os.path.join(path1,path2))
+ff = reconstruction(os.path.join(path1,path2))
+io.imsave('reconstruction.jpg', ff)
